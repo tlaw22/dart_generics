0ec97b777dbfeeb6c42c33cb538fac0486ef4f6c
tim <timothy.lawless@gmail.com>
2024-01-31 17:04:55 -0500

    added notes

--- bin/string_generics.dart
+++ bin/string_generics.dart
@@ -1,3 +1,5 @@
+// Create a generic list class that holds String data and allows us to push vaules on to the list. Create a pop method that removes the last entry.
+
 class StringGenerics {
   List <String> list = [];
   push(String newElemenet) {
